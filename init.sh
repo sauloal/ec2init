6582d58f0adc9730f38acd4241608e662d091e14
saulo <sauloal@gmail.com>
2013-04-13 04:42:15 +0300

    ec2 vars

--- init.sh
+++ init.sh
@@ -5,6 +5,16 @@ BASE=/mnt/external/init
 #install git
 su -c 'yum install -y git'
 
+EC2_HOSTNAME=`curl http://169.254.169.254/latest/meta-data/hostname`
+
+EC2_PRIV_HOSTNAME=`curl http://169.254.169.254/latest/meta-data/local-hostname`
+EC2_PRIV_IPV4=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+
+EC2_PUB_IPV4=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+EC2_PUB_HOSTNAME=`curl http://169.254.169.254/latest/meta-data/public-hostname`
+EC2_TYPE=`curl http://169.254.169.254/latest/meta-data/instance-type`
+
+
 #pull updates
 cd $BASE
 git pull
