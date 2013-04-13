05c62eb10406c6ca7f3f4263ecd7f922c1d1ed02
saulo <sauloal@gmail.com>
2013-04-13 05:48:38 +0300

    Update init.sh.00.install.sh

--- init.sh.00.install.sh
+++ init.sh.00.install.sh
@@ -1,11 +1,19 @@
 #what to install
 echo "RUNNING INSTALL"
+#misc
 yum install -y wget
 yum install -y byobu
 yum install -y which
 yum install -y nano
 yum install -y cloud-utils
+#apache
 yum install -y httpd
+#samba
 yum install -y samba samba-client
+#nfs
 yum install -y nfs-utils
+#vpn
+yum install -y bridge-utils
+yum install -y openvpn
+#iscsi
 yum install -y libiscsi-utils.i686 scsi-target-utils.i686 lsscsi.i686
