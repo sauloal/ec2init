357090d8c9d006db1eb421d37cd8140ac41ef270
saulo <sauloal@gmail.com>
2013-04-13 05:11:50 +0300

    http

--- init.sh.00.install.sh
+++ init.sh.00.install.sh
@@ -4,4 +4,6 @@ yum install -y wget
 yum install -y byobu
 yum install -y which
 yum install -y nano
+yum install -y cloud-utils
+yum install -y httpd
 yum install -y libiscsi-utils.i686 scsi-target-utils.i686 lsscsi.i686
