4df89f12805afcb86a6ead2e18e36bcae2c98712
saulo <sauloal@gmail.com>
2013-04-14 01:23:21 +0300

    Update init.sh.00.install.sh

--- init.sh.00.install.sh
+++ init.sh.00.install.sh
@@ -26,7 +26,7 @@ yum install -y openvpn
 yum install -y libiscsi-utils scsi-target-utils lsscsi
 
 #management
-unset -xe
+set +xe
 wget -O /tmp/webmin-1.620-1.noarch.rpm 'http://downloads.sourceforge.net/project/webadmin/webmin/1.620/webmin-1.620-1.noarch.rpm?r=http%3A%2F%2Fwww.webmin.com%2Fdownload.html&ts=1365823599&use_mirror=surfnet'
 yum install -y /tmp/webmin-1.620-1.noarch.rpm
 rm -f /tmp/webmin-1.620-1.noarch.rpm
