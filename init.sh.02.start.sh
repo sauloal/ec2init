c8200e251198409af5a4ce82bc82cffc35f38eea
saulo <sauloal@gmail.com>
2013-04-13 05:12:17 +0300

    httpd

--- init.sh.02.start.sh
+++ init.sh.02.start.sh
@@ -1,5 +1,9 @@
 #which services to start
 echo "RUNNING START"
+
+/sbin/chkconfig httpd on
+/sbin/service httpd start
+
 systemctl enable tgtd.service
 systemctl start tgtd.service
 systemctl status tgtd.service
