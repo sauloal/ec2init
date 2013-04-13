d260a0f1244b4e31771f527d49a653b5df487008
saulo <sauloal@gmail.com>
2013-04-13 06:14:19 +0300

    Update init.sh.00.install.sh

--- init.sh.00.install.sh
+++ init.sh.00.install.sh
@@ -17,3 +17,7 @@ yum install -y bridge-utils
 yum install -y openvpn
 #iscsi
 yum install -y libiscsi-utils.i686 scsi-target-utils.i686 lsscsi.i686
+#management
+yum install -y http://prdownloads.sourceforge.net/webadmin/webmin-1.620-1.noarch.rpm
+#TODO
+#git clone https://github.com/bobsta63/zpanelx.git zpanelx
