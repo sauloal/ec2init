b480df150cf1fea60376e654b8c123361dcca5a4
saulo <sauloal@gmail.com>
2013-04-13 05:24:05 +0300

    samba & nfs

--- init.sh.00.install.sh
+++ init.sh.00.install.sh
@@ -6,4 +6,6 @@ yum install -y which
 yum install -y nano
 yum install -y cloud-utils
 yum install -y httpd
+yum install -y samba samba-client
+yum install -y nfs-utils
 yum install -y libiscsi-utils.i686 scsi-target-utils.i686 lsscsi.i686
