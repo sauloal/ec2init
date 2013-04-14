9f7e470bf3a5db561f7f71d43802c72a5b595ea5
saulo <sauloal@gmail.com>
2013-04-14 05:39:17 +0300

    Update init.sh.30.python.sh

--- init.sh.30.python.sh
+++ init.sh.30.python.sh
@@ -13,3 +13,5 @@ easy_install Flask
 easy_install simplejson
 easy_install jsonpickle
 
+# zdaemon - http://ridingpython.blogspot.nl/2011/08/turning-your-python-script-into-linux.html
+easy_install zdaemon
