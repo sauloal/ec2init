60a195748413b9fbe5782d6c028db4fdde4e027e
saulo <sauloal@gmail.com>
2013-04-15 13:18:25 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,13 +9,13 @@ BASE=~/ec2init
 LOG="init.sh."`date +%Y%m%d_%H%M%S`".log"
 
 #Saves file descriptors so they can be restored to whatever they were before redirection or used themselves to output to whatever they were before the following redirect.
-exec 3>&1 4>&2
+#exec 3>&1 4>&2
 
 #Restore file descriptors for particular signals. Not generally necessary since they should be restored when the sub-shell exits.
-trap 'exec 2>&4 1>&3' 0 1 2 3
+#trap 'exec 2>&4 1>&3' 0 1 2 3
 
 #Redirect stdout to file log.out then redirect stderr to stdout. Note that the order is important when you want them going to the same file. stdout must be redirected before stderr is redirected to stdout.
-exec 1>${LOG} 2>&1
+#exec 1>${LOG} 2>&1
 
 
 ##########
@@ -29,6 +29,8 @@ sleep 5
 ###########
 #run scripts
 ###########
+(
 source $BASE/begin.sh
+) | tee $LOG
 
 echo "DONE INIT"
