2d2acb7df575b086d7059927689f6f7c06089fde
saulo <sauloal@gmail.com>
2013-04-16 04:20:05 +0300

    Update init.sh.01.mods.sh

--- init.sh.01.mods.sh
+++ init.sh.01.mods.sh
@@ -1,7 +1,7 @@
 #modifications to setup
 
 if [[ 0 -eq 1 ]]; then
-
+echo mods
 ##################
 # iSCSI
 ##################
@@ -103,6 +103,11 @@ if [[ 0 -eq 1 ]]; then
 
 fi
 
+
+
+
+
+
 if [[ ! -e "$EC2_EXTERNAL_DST" ]]; then
   mkdir $EC2_EXTERNAL_DST
 fi
@@ -114,4 +119,35 @@ if [[ ! -z `mount | grep "$EC2_EXTERNAL_DST"` ]]; then
 else
 	echo "external $EC2_EXTERNAL_SRC to $EC2_EXTERNAL_DST already mounted"
 fi
+
+
+
+
+#https://wiki.archlinux.org/index.php/Sshfs
+#http://ubuntuforums.org/showthread.php?t=1182295
+#TODO:
+# /etc/passwd:
+# user:1003:1003:User,,,:/:/usr/sbin/nologin
+#
+#/etc/group:
+#sftp:1004:user
+#
+#/etc/ssh/sshd_config:
+# Logging
+#SyslogFacility AUTH
+#LogLevel DEBUG
+#Subsystem sftp internal-sftp
+#Match group sftp
+#ForceCommand internal-sftp
+#ChrootDirectory /var/sshbox
+
+#http://serverfault.com/questions/431169/is-it-possible-to-allow-key-based-authentication-for-sshd-cofnig-chroot-sftp-use
+#OR:
+#Match Group sftpusers
+#        ChrootDirectory /sftp/%u
+#        ForceCommand internal-sftp
+#        PubkeyAuthentication yes
+#        AuthorizedKeysFile     %h/.ssh/authorized_keys
+
+cp --no-preserve=all mods/sshd_config.new /etc/ssh/sshd_config
 
