7c6e6b3f6068fa84dbf3d50d9361068d9f0a1818
saulo <sauloal@gmail.com>
2013-04-16 19:18:51 +0300

    Update init.sh.02.start.sh

--- init.sh.02.start.sh
+++ init.sh.02.start.sh
@@ -6,38 +6,4 @@ systemctl enable httpd.service
 systemctl start  httpd.service
 systemctl status httpd.service
 
-#iSCSI
-#systemctl enable tgtd.service
-#systemctl start  tgtd.service
-#systemctl status tgtd.service
 
-#SAMBA
-#http://www.howtoforge.com/fedora-18-samba-standalone-server-with-tdbsam-backend
-#systemctl enable smb.service
-#systemctl start  smb.service
-#systemctl status smb.service
-
-#NFS
-#http://www.learnbydoingit.org/2013/01/configuring-nfs-server-on-fedora-18/
-#systemctl start  rpcbind.service
-#systemctl enable rpcbind.service
-#systemctl status rpcbind.service
-
-#systemctl start  nfs-server.service
-#systemctl enable nfs-server.service
-#systemctl status nfs-server.service
-
-#systemctl start  nfs-lock.service
-#systemctl enable nfs-lock.service
-#systemctl status nfs-lock.service
-
-#systemctl start  nfs-idmap.service
-#systemctl enable nfs-idmap.service
-#systemctl status nfs-idmap.service
-
-#WEBMIN - CHANGE PASSWORD ASAP
-#port 10000
-/sbin/chkconfig webmin on
-set +ex
-service webmin start &
-set -ex
