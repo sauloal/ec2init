e5ef96d81d2a00a6ccf1509f6f3af7c6260a2c46
saulo <sauloal@gmail.com>
2013-04-16 19:14:18 +0300

    cleanup

--- init.sh.00.install.sh
+++ init.sh.00.install.sh
@@ -18,57 +18,6 @@ yum install -y patch
 ###############
 #yum install -y httpd
 
-###############
-#samba
-###############
-yum install -y samba samba-client
-
-###############
-#nfs
-###############
-#yum install -y nfs-utils
-
-###############
-#vpn
-###############
-#yum install -y bridge-utils
-#yum install -y openvpn
-
-###############
-#iscsi
-###############
-#yum install -y libiscsi-utils scsi-target-utils lsscsi
-#yum install -y ntfs-3g ntfsprogs
-
-###############
-#management - webmin
-###############
-set +xe
-
-WEBMINTMP=/tmp/webmin-1.620-1.noarch.rpm
-if [[ ! -f "${WEBMINTMP}" ]]; THEN
-  wget -O $WEBMINTMP 'http://downloads.sourceforge.net/project/webadmin/webmin/1.620/webmin-1.620-1.noarch.rpm?r=http%3A%2F%2Fwww.webmin.com%2Fdownload.html&ts=1365823599&use_mirror=surfnet'
-  yum install -y $WEBMINTMP
-  #rm -f $WEBMINTMP
-  #http://prdownloads.sourceforge.net/webadmin/webmin-1.620-1.noarch.rpm
-fi
-
-set -xe
-yum install -y perl make openssl perl-Authen-PAM
-
-
-###############
-#ownclound
-#http://software.opensuse.org/download/package?project=isv:ownCloud:community&package=owncloud
-#http://ubuntuserverguide.com/2013/04/how-to-setup-owncloud-server-5-with-ssl-connection.html
-###############
-cd /etc/yum.repos.d/
-wget http://download.opensuse.org/repositories/isv:ownCloud:community/Fedora_18/isv:ownCloud:community.repo
-yum install owncloud
-service httpd restart
-
-
-
 
 #TODO
 #git clone https://github.com/bobsta63/zpanelx.git zpanelx
