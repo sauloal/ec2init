236a0fe981a18764d72fd75ab675fade037feca4
saulo <sauloal@gmail.com>
2013-04-17 01:36:48 +0300

    Update init.sh.01.install.05.ajaxplorer.sh

--- init.sh.01.install.05.ajaxplorer.sh
+++ init.sh.01.install.05.ajaxplorer.sh
@@ -3,3 +3,8 @@ rpm -Uvh http://dl.ajaxplorer.info/repos/el6/ajaxplorer-stable/ajaxplorer-releas
 
 yum update -y
 yum install -y ajaxplorer
+
+cp -f --no-preserve=all mods/bootstrap_conf.php /etc/ajaxplorer/bootstrap_conf.php 
+cp -f --no-preserve=all mods/ajaxplorer.conf /etc/httpd/conf.d/ajaxplorer.conf
+
+/bin/systemctl restart  httpd.service
