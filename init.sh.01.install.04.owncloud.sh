d1a14590c4dd2bc4352b19bba2e969e50f37a773
saulo <sauloal@gmail.com>
2013-04-16 22:33:14 +0300

    Update init.sh.01.install.04.owncloud.sh

--- init.sh.01.install.04.owncloud.sh
+++ init.sh.01.install.04.owncloud.sh
@@ -3,6 +3,7 @@
 #http://software.opensuse.org/download/package?project=isv:ownCloud:community&package=owncloud
 #http://ubuntuserverguide.com/2013/04/how-to-setup-owncloud-server-5-with-ssl-connection.html
 ###############
+set -xeu
 cd /etc/yum.repos.d/
 wget http://download.opensuse.org/repositories/isv:ownCloud:community/Fedora_18/isv:ownCloud:community.repo
 yum install -y owncloud
@@ -10,7 +11,7 @@ service httpd restart
 
 
 setenforce 0
-cp --no-preserve=all mods/selinux.new /etc/sysconfig/selinux
+cp -f --no-preserve=all mods/selinux.new /etc/sysconfig/selinux
 
 
 #################
@@ -31,7 +32,7 @@ else
   echo "owncloud already in fstab"
 fi
 
-if [[ ! -z `mount | grep "owncloud"` ]]; then
+if [[ -z `mount | grep "owncloud"` ]]; then
   echo "mounting owncloud"
   mount --bind $EC2_EXTERNAL_DST/owncloud/data /var/www/html/owncloud/data
   mount --make-shared /var/www/html/owncloud/data
