5c250c8baa8c254b1d5df5f3007a3ee447accd7e
saulo <sauloal@gmail.com>
2013-04-17 13:29:13 +0300

    Update init.sh.30.python.sh

--- init.sh.30.python.sh
+++ init.sh.30.python.sh
@@ -15,3 +15,5 @@ easy_install jsonpickle
 
 # zdaemon - http://ridingpython.blogspot.nl/2011/08/turning-your-python-script-into-linux.html
 easy_install zdaemon
+easy_install pip
+pip install Glances
