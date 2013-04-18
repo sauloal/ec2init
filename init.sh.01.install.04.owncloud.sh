2a88b3bab5a08c39472dda4fadb563c1193d02ac
saulo <sauloal@gmail.com>
2013-04-18 17:17:20 +0300

    Update init.sh.01.install.04.owncloud.sh

--- init.sh.01.install.04.owncloud.sh
+++ init.sh.01.install.04.owncloud.sh
@@ -18,25 +18,31 @@ cp -f --no-preserve=all mods/selinux.new /etc/sysconfig/selinux
 #################
 # OwnCloud
 #################
-if [[ ! -e "$EC2_EXTERNAL_DST/owncloud" ]]; then
-  mkdir -p $EC2_EXTERNAL_DST/owncloud/data
-  chown -R apache:apache $EC2_EXTERNAL_DST/owncloud/data
-  chown -R apache:apache /var/www/html/owncloud
-  chmod 0774 $EC2_EXTERNAL_DST/owncloud
-  chmod 0774 $EC2_EXTERNAL_DST/owncloud/data
-fi
-
-if [[ -z `grep owncloud /etc/fstab` ]]; then
-  echo "adding owncloud to fstab"
-  echo "$EC2_EXTERNAL_DST/owncloud/data   /var/www/html/owncloud/data/        bind    bind    0" >> /etc/fstab
+if [[ ! -z "$EC2_EXTERNAL_PRESENT" ]]; then
+  echo "mounting owncloud to external"
+  if [[ ! -e "$EC2_EXTERNAL_DST/owncloud" ]]; then
+    mkdir -p $EC2_EXTERNAL_DST/owncloud/data
+    chown -R apache:apache $EC2_EXTERNAL_DST/owncloud/data
+    chown -R apache:apache /var/www/html/owncloud
+    chmod 0774 $EC2_EXTERNAL_DST/owncloud
+    chmod 0774 $EC2_EXTERNAL_DST/owncloud/data
+  fi
+  
+  if [[ -z `grep owncloud /etc/fstab` ]]; then
+    echo "adding owncloud to fstab"
+    echo "$EC2_EXTERNAL_DST/owncloud/data   /var/www/html/owncloud/data/        bind    bind    0" >> /etc/fstab
+  else
+    echo "owncloud already in fstab"
+  fi
+  
+  if [[ -z `mount | grep "owncloud"` ]]; then
+    echo "mounting owncloud"
+    mount --bind $EC2_EXTERNAL_DST/owncloud/data /var/www/html/owncloud/data
+    mount --make-shared /var/www/html/owncloud/data
+  else
+    echo "owncloud already mounted"
+  fi
+  echo "mounting owncloud to external done"
 else
-  echo "owncloud already in fstab"
-fi
-
-if [[ -z `mount | grep "owncloud"` ]]; then
-  echo "mounting owncloud"
-  mount --bind $EC2_EXTERNAL_DST/owncloud/data /var/www/html/owncloud/data
-  mount --make-shared /var/www/html/owncloud/data
-else
-  echo "owncloud already mounted"
+  echo "external not present. skipping mounting owncloud to external"
 fi
