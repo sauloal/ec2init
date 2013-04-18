c2328f0b0f39365976ba935c03573086ec133bc6
saulo <sauloal@gmail.com>
2013-04-18 17:15:23 +0300

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -16,6 +16,12 @@ EC2_TYPE=`curl http://169.254.169.254/latest/meta-data/instance-type`
 # http://stackoverflow.com/questions/4249488/find-region-from-within-ec2-instance
 EC2_REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
 
+if [[ ! -z "$EC2_EXTERNAL_SRC" ]]; then
+	EC2_EXTERNAL_PRESENT=`fdisk -l | grep $EC2_EXTERNAL_SRC`
+else
+	EC2_EXTERNAL_PRESENT=""
+fi
+
 echo "HOSTNAME          $EC2_HOSTNAME"
 echo "PRIV HOSTNAME     $EC2_PRIV_HOSTNAME"
 echo "PRIV IPV4         $EC2_PRIV_IPV4"
@@ -26,26 +32,29 @@ echo "EC2  ARN          $EC2_ARN"
 echo "EC2  REGION       $EC2_REGION"
 echo "EC2  EXTERNAL SRC $EC2_EXTERNAL_SRC"
 echo "EC2  EXTERNAL DST $EC2_EXTERNAL_DST"
+echo "EC2  EXTERNAL PRE $EC2_EXTERNAL_PRESENT"
 
 
-echo "export EC2_HOSTNAME=$EC2_HOSTNAME"            > ~/.ec2
-echo "export EC2_PRIV_HOSTNAME=$EC2_PRIV_HOSTNAME" >> ~/.ec2
-echo "export EC2_PRIV_IPV4=$EC2_PRIV_IPV4"         >> ~/.ec2
-echo "export EC2_PUB_IPV4=$EC2_PUB_IPV4"           >> ~/.ec2
-echo "export EC2_PUB_HOSTNAME=$EC2_PUB_HOSTNAME"   >> ~/.ec2
-echo "export EC2_TYPE=$EC2_TYPE"                   >> ~/.ec2
-echo "export EC2_ARN=$EC2_ARN"                     >> ~/.ec2
-echo "export EC2_REGION=$EC2_REGION"               >> ~/.ec2
-echo "export EC2_EXTERNAL_SRC=$EC2_EXTERNAL_SRC"   >> ~/.ec2
-echo "export EC2_EXTERNAL_DST=$EC2_EXTERNAL_DST"   >> ~/.ec2
+echo "export EC2_HOSTNAME=$EC2_HOSTNAME"                  > ~/.ec2
+echo "export EC2_PRIV_HOSTNAME=$EC2_PRIV_HOSTNAME"       >> ~/.ec2
+echo "export EC2_PRIV_IPV4=$EC2_PRIV_IPV4"               >> ~/.ec2
+echo "export EC2_PUB_IPV4=$EC2_PUB_IPV4"                 >> ~/.ec2
+echo "export EC2_PUB_HOSTNAME=$EC2_PUB_HOSTNAME"         >> ~/.ec2
+echo "export EC2_TYPE=$EC2_TYPE"                         >> ~/.ec2
+echo "export EC2_ARN=$EC2_ARN"                           >> ~/.ec2
+echo "export EC2_REGION=$EC2_REGION"                     >> ~/.ec2
+echo "export EC2_EXTERNAL_SRC=$EC2_EXTERNAL_SRC"         >> ~/.ec2
+echo "export EC2_EXTERNAL_DST=$EC2_EXTERNAL_DST"         >> ~/.ec2
+echo "export EC2_EXTERNAL_PRESENT=$EC2_EXTERNAL_PRESENT" >> ~/.ec2
 
 if [[ -z `grep /.ec2  ~/.bashrc` ]]; then 
-echo 'modifying bashrc'
-echo "source ~/.ec2"          >> ~/.bashrc
-echo "PATH=$PATH:$BASE/tools" >> ~/.bashrc
+	echo 'modifying bashrc'
+	echo "source ~/.ec2"          >> ~/.bashrc
+	echo "PATH=$PATH:$BASE/tools" >> ~/.bashrc
 
 else
-grep /.ec2  ~/.bashrc
+	echo 'bashrc already modified'
+	grep /.ec2  ~/.bashrc
 fi
 
 cp ~/.ec2    ~ec2-user/
@@ -54,8 +63,12 @@ cp ~/.bashrc ~ec2-user/
 
 for file in $BASE/init.sh.*.sh; do
 	if [[ ! -e "${file}.skip" ]]; then
+		echo "running $file"
 		source $file
 		touch ${file}.skip
+		echo "running $file done"
+	else
+		echo "$file already run. skipping"
 	fi
 done
 
