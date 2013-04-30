8570f3f6b54e52aed1fc0ce1bd3af6d840a9aecb
root <root@ip-10-229-65-94.eu-west-1.compute.internal>
2013-04-30 06:51:12 -0400

    pubkeys

--- init.sh.03.users.sh
+++ init.sh.03.users.sh
@@ -1,10 +1,22 @@
-useradd -s /sbin/nologin guests
 
-if [[ ! -d "~/guests/.ssh" ]]; then
-  mkdir ~/guests/.ssh
+if [[ ! -d "/home/guests" ]]; then
+	echo "adding guests user"
+	useradd -s /sbin/nologin guests
+else
+	echo "guests user already exists"
 fi
 
-cp mods/authorized_keys ~/guests/.ssh
 
-chmod 400 ~/guests/.ssh
-chmod 400 ~/guests/.ssh/authorized_keys
+if [[ ! -d "/home/guests/.ssh" ]]; then
+	echo "creating .ssh folder"
+ 	mkdir ~guests/.ssh
+else
+	echo ".ssh folder already exists"
+fi
+
+echo copying authorized keys
+cp mods/authorized_keys ~guests/.ssh
+
+echo changing permission of authorized keys
+chmod 400 ~guests/.ssh
+chmod 400 ~guests/.ssh/authorized_keys
