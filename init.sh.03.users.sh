561ba49f89b6196b0afd5066a6bf21f131b7f50d
saulo <sauloal@gmail.com>
2013-04-30 13:46:38 +0300

    Update init.sh.03.users.sh

--- init.sh.03.users.sh
+++ init.sh.03.users.sh
@@ -1,1 +1,10 @@
 useradd -s /sbin/nologin guests
+
+if [[ ! -d "~/guests/.ssh" ]]; then
+  mkdir ~/guests/.ssh
+fi
+
+cp mods/authorized_keys ~/guests/.ssh
+
+chmod 400 ~/guests/.ssh
+chmod 400 ~/guests/.ssh/authorized_keys
