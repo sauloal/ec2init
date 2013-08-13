7e8bb6716576d084652ae39d86c2f29be374c071
saulo <sauloal@gmail.com>
2013-08-13 14:03:44 +0200

    Update init.sh.32.btsync.sh

--- init.sh.32.btsync.sh
+++ init.sh.32.btsync.sh
@@ -25,6 +25,7 @@ fi
 cp ~/.sync.json /home/$DEFAULT_USER/
 chmod 400 ~/.sync.json
 chmod 400 /home/$DEFAULT_USER/.sync.json
+chown $DEFAULT_USER:$DEFAULT_USER /home/$DEFAULT_USER/.sync.json
 
 
 systemctl --system daemon-reload
