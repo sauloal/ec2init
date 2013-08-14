a76533a64a41a1bb1dba5a9216a9bfdad92ae949
saulo <sauloal@gmail.com>
2013-08-14 16:02:59 +0200

    Update init.sh.32.btsync.sh

--- init.sh.32.btsync.sh
+++ init.sh.32.btsync.sh
@@ -6,30 +6,50 @@ tar xvf btsync.tar.gz
 
 cp btsync /usr/bin/btsync
 
-if [[ ! -f "/etc/rc.d/init.d/btsyncd" ]]; then
+BT_CONF_FOLDER="EC2_EXTERNAL_CONFIG_DST/bittorrentsync"
+SYNC_JSON=$BT_CONF_FOLDER/.sync.json
+BTSYNC_D=/etc/init.d/btsyncd
+
+if [[ ! -f "$BTSYNC_D" ]]; then
 	echo "copying btsyncd"
-	sed 's/<DEFAULT_USER>/apache/g' mods/btsyncd > /etc/rc.d/init.d/btsyncd
+	sed -e 's/<EFFECTIVE_USER>/apache/g' -e 's@<CONFIG_FILE>@'$SYNC_JSON'@g' mods/btsyncd > $BTSYNC_D
 else
 	echo "btsyncd already present"
 fi
+chmod +x $BTSYNC_D
 
-chmod +x /etc/init.d/btsyncd
 
-if [[ ! -f "~/.sync.json" ]]; then
+
+if [[ ! -d "$BT_CONF" ]]; then
+	mkdir $BT_CONF_FOLDER
+	chown root:apache $BT_CONF_FOLDER
+  	chmod 770 $BT_CONF_FOLDER
+fi
+
+
+
+if [[ ! -d "$EXTERNAL_SYNC_FOLDER" ]]; then
+  mkdir $EXTERNAL_SYNC_FOLDER
+  chown root:apache $EXTERNAL_SYNC_FOLDER
+  chmod 770 $EXTERNAL_SYNC_FOLDER
+fi
+
+
+if [[ ! -f "$SYNC_JSON" ]]; then
 	echo "copying sync.json"
-	sed -e 's/<BT_USER>/'$BTSYNC_USER'/g' -e 's/<BT_PASS>/'$BTSYNC_PASS'/g' -e 's@<EC2_EXTERNAL_CONFIG_DST>@'$EC2_EXTERNAL_CONFIG_DST'@g' mods/btsync.json  >  ~/.sync.json
+	sed -e 's/<BT_USER>/'$BTSYNC_USER'/g' -e 's/<BT_PASS>/'$BTSYNC_PASS'/g' -e 's@<DATA_FOLDER>@'$BT_CONF_FOLDER'@g' mods/btsync.json  >  $SYNC_JSON
 else
 	echo "sync.json already present"
 fi
 
-cp ~/.sync.json /home/$DEFAULT_USER/
-cp ~/.sync.json /etc/
+#cp ~/.sync.json /home/$DEFAULT_USER/
+#cp ~/.sync.json /etc/
 
-chmod 440 ~/.sync.json
-chmod 440 /home/$DEFAULT_USER/.sync.json
-chmod 440 /etc/.sync.json
-chown $DEFAULT_USER:apache /home/$DEFAULT_USER/.sync.json
-chown $DEFAULT_USER:apache /etc/.sync.json
+chmod 440 $SYNC_JSON
+#chmod 440 /home/$DEFAULT_USER/.sync.json
+#chmod 440 /etc/.sync.json
+#chown $DEFAULT_USER:apache /home/$DEFAULT_USER/.sync.json
+chown $DEFAULT_USER:apache $SYNC_JSON
 
 
 systemctl --system daemon-reload
