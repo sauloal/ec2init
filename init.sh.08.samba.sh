be7d2e057ff67ff47970c6891dfda832bc5aeccd
Saulo Alves <sauloal@gmail.com>
2013-08-25 22:15:30 +0000

    samba

---
+++ init.sh.08.samba.sh
@@ -0,0 +1,11 @@
+yum install -yt samba
+yum install -yt samba-client
+
+mv /etc/samba/smb.conf /etc/samba/smb.conf.bkp
+cp smb.conf.new /etc/samba/smb.conf
+
+service smb start
+service nmb start
+
+service smb restart
+service nmb restart
