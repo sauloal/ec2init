003a5cce0a6285cdd86b7f39caa60ade2426a6e2
Saulo Alves <sauloal@gmail.com>
2013-09-24 01:04:41 +0000

    smb

--- init.sh.08.samba.sh
+++ init.sh.08.samba.sh
@@ -2,7 +2,7 @@ yum install -yt samba
 yum install -yt samba-client
 
 mv /etc/samba/smb.conf /etc/samba/smb.conf.bkp
-cp smb.conf.new /etc/samba/smb.conf
+cp ~/ec2init/mods/smb.conf.new /etc/samba/smb.conf
 
 service smb start
 service nmb start
